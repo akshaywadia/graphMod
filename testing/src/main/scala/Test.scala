07a504c3bf19694aaadef52cd68dcd6cc3c2a553
Ubuntu <ubuntu@ip-172-31-4-222.us-west-1.compute.internal>
2015-02-12 07:02:28 +0000

    Testing on fresh spark install.

--- testing/src/main/scala/Test.scala
+++ testing/src/main/scala/Test.scala
@@ -6,6 +6,7 @@ import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 import org.apache.log4j.Logger 
 import org.apache.log4j.Level 
+import scala.util.Random
 
 
 
@@ -14,6 +15,7 @@ object Test {
     // logging off
     Logger.getLogger("org").setLevel(Level.OFF) 
     Logger.getLogger("akka").setLevel(Level.OFF) 
+    val rand = new Random()
     // Create SparkContext -- boilerplate
     val conf = new SparkConf().setMaster("local[4]").setAppName("Test")
     val sc = new SparkContext(conf)
@@ -27,25 +29,39 @@ object Test {
 
       val gmod = new graphMod()
       var postrun = gmod.run(gmod.initVattr(gr))
-      println("First run...")
+      println("First run over ...")
       //val edRdd = sc.textFile("/user/ed.txt")
       //var g = gmod.updateEdgeBatch(edRdd, postrun)
 
 
       //g.edges.saveAsTextFile("/user/akshay/delta/batch")
-      println("Starting reset")
-      val modgr = gmod.resetGraph(postrun)
+      var i : Int = 0
+      while (i < 10) {
+      println("Stage " + i.toString + " Starting reset")
+      var modgr = gmod.resetGraph(postrun)
+      
       //var count = modgr.vertices.map{ case (vid,vattr) => vattr.disturbed}.reduce((a,b) => a+b)
       //println("first run count: " + count.toString)
-      var gup = gmod.updateEdge("143 286 500.0", modgr)
-      println("Staring second run...")
-      var postup = gmod.run(gup, true)
+      
+      // pick random edge, by selecting a random vertex, and a random
+      // bit for left/right child
+      var src = rand.nextInt(510) + 1
+      var dst = if (rand.nextBoolean()) (2*src) else (2*src + 1)
+      var wt = 2*i
+      var ed = src.toString + " " + dst.toString + " " + wt.toDouble
+
+      var gup = gmod.updateEdge(ed, modgr)
+      println("Starting second run...")
+      postrun = gmod.run(gup)
       println("Second run over...")
-      var deltav = postup.vertices.filter{ case (vid,vattr) => vattr.disturbed != 0}
-      println("Post count: " + deltav.count())  
-      postup.vertices.saveAsTextFile("/user/akshay/delta/v")
-      postup.edges.saveAsTextFile("/user/akshay/delta/e")
-      deltav.saveAsTextFile("/user/akshay/delta/dv")
+      i += 1
+      }
+      println("\n")
+//      var deltav = postup.vertices.filter{ case (vid,vattr) => vattr.disturbed != 0}
+ //     println("Post count: " + deltav.count())  
+//      postup.vertices.saveAsTextFile("/user/akshay/delta/v")
+ //     postup.edges.saveAsTextFile("/user/akshay/delta/e")
+  //    deltav.saveAsTextFile("/user/akshay/delta/dv")
 
 /////////// working debug /////////////////
 /*      val gmod = new graphMod()
